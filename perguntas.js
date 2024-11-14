9b31fe4a603ae1b4233962d71dac8ae862d13508
Programertester <nahidwinp@gmail.com>
2024-11-13 22:42:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'PROFESSORES',
+    'Qual o melhor professor do Chanceler?',
+    'Prof. Vinicius'
+)
+
+criaCartao(
+    'Classe',
+    'Qual o MELHOR Terceirão?',
+    '3°A (Obvio nekkkk)'
+)
+
+criaCartao(
+    'Vai dar Namoro!',
+    'Quem é meu crush (DANI)?',
+    'Samuel, meu TIGRÃO!'
+)
+
+criaCartao(
+    'Gamezin',
+    'Qual meu Jogo preferido?',
+    'Call Of Dury Mobile'
+)
